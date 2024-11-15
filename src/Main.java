ac03db63eae6710a3a790f2df53572bcb65d541c
Yuan-0210 <houyuan0210@gmail.com>
2024-11-15 13:05:23 +0800

    testing

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+package src.tools.Person;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Miama", 10);
+        person.display();
+    }
+}
